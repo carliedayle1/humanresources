17d65f33b0e7f76e9e5aa987e361e90a7f9ce01f
Erick Dayle Loon <daylecarlie@gmail.com>
2020-12-14 09:17:33 +0800

    Minor changes

--- frontend/src/screens/UserProfileScreen.js
+++ frontend/src/screens/UserProfileScreen.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const UserProfileScreen = ({ history }) => {
-
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
@@ -15,7 +14,7 @@ const UserProfileScreen = ({ history }) => {
     if (!userInfo) {
       history.push("/");
     }
-  }, [history])
+  }, [history, userInfo]);
 
   return (
     <>
